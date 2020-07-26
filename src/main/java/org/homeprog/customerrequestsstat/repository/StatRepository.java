dbf11d7bd8dc78e808f08ba8730311f8694eb22e
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-07-26 20:33:18 +0200

    Adding new queries

--- src/main/java/org/homeprog/customerrequestsstat/repository/StatRepository.java
+++ src/main/java/org/homeprog/customerrequestsstat/repository/StatRepository.java
@@ -3,13 +3,12 @@ package org.homeprog.customerrequestsstat.repository;
 import org.homeprog.customerrequestsstat.entity.StatCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
+import java.time.Instant;
 import java.util.List;
 
 public interface StatRepository extends JpaRepository<StatCustomer, Long> {
-    /*@Query(value = "SELECT * FROM request_stats r WHERE r.customer_id = 'customerId' AND DATE (time) = 'dateAndTime'",
-            nativeQuery=true)
-    List<StatCustomer> findAllByCustomerIdAndTime(@Param("customer_id") Long customerId, @Param("time") Date dateAndTime);*/
-
-    List<StatCustomer> findAllByCustomer_IdAndTime(Long customerId, Date dateAndTime);
+    List<StatCustomer> findAllStatCustomersByCustomerIdAndTimeBetween(Long id, Instant dateFrom, Instant dateTo);
+    List<StatCustomer> findAllStatCustomersByCustomerIdAndIsValid(Long id, Boolean isValid);
+    List<StatCustomer> findAllStatCustomersByCustomerIdAndIsValidAndTimeBetween
+                       (Long id, Boolean isValid, Instant dateFrom, Instant dateTo);
 }
