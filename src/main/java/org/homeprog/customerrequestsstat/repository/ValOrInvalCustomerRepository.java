da54581d6aed19e18890260ceb5c4cbff33d8575
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-07-26 20:24:28 +0200

    Adding Swagger

--- src/main/java/org/homeprog/customerrequestsstat/repository/ValOrInvalCustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.homeprog.customerrequestsstat.repository;
-
-import org.homeprog.customerrequestsstat.entity.ValOrInvalCustomer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValOrInvalCustomerRepository extends JpaRepository<ValOrInvalCustomer, Long> {
-
-}
