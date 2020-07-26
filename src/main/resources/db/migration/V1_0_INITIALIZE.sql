9cbebad453b9cee4bf50eb6b079cb24992f0d7f7
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-07-26 20:36:15 +0200

    Altering properties of time field of request_stats table

--- src/main/resources/db/migration/V1_0_INITIALIZE.sql
+++ src/main/resources/db/migration/V1_0_INITIALIZE.sql
@@ -9,7 +9,7 @@ CREATE TABLE customer (
 CREATE TABLE request_stats (
     id int(11) unsigned NOT NULL AUTO_INCREMENT,
     customer_id int(11) unsigned NOT NULL,
-    time datetime DEFAULT CURRENT_TIMESTAMP,
+    time datetime NOT NULL,
     is_valid boolean DEFAULT true,
     PRIMARY KEY (id),
     UNIQUE KEY unique_customer_time (customer_id,time),
