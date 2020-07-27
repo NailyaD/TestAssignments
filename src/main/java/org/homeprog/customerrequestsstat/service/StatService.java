19286808d5c4769db7448bf70ff6a093d9c30b8d
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-07-27 19:04:52 +0200

    Adding validation method for addStatCustomer method

--- src/main/java/org/homeprog/customerrequestsstat/service/StatService.java
+++ src/main/java/org/homeprog/customerrequestsstat/service/StatService.java
@@ -1,6 +1,11 @@
 package org.homeprog.customerrequestsstat.service;
 
+import org.homeprog.customerrequestsstat.entity.Customer;
 import org.homeprog.customerrequestsstat.entity.StatCustomer;
+import org.homeprog.customerrequestsstat.exception.CustomerNotFoundException;
+import org.homeprog.customerrequestsstat.repository.BlackListIPRepository;
+import org.homeprog.customerrequestsstat.repository.BlackListUserAgentRepository;
+import org.homeprog.customerrequestsstat.repository.CustomerRepository;
 import org.homeprog.customerrequestsstat.repository.StatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +17,26 @@ import java.util.List;
 public class StatService {
 
     private final StatRepository statRepo;
+    private final CustomerRepository customerRepo;
+    private final BlackListIPRepository blackListIPRepo;
+    private final BlackListUserAgentRepository blackListUARepo;
 
     @Autowired
-    public StatService(StatRepository statRepo) {
+    public StatService(StatRepository statRepo, CustomerRepository customerRepo,
+                       BlackListIPRepository blackListIPRepo, BlackListUserAgentRepository blackListUARepo) {
         this.statRepo = statRepo;
+        this.customerRepo = customerRepo;
+        this.blackListIPRepo = blackListIPRepo;
+        this.blackListUARepo = blackListUARepo;
     }
 
-    public StatCustomer addStatCustomer(StatCustomer statCustomer) {
+    public StatCustomer addStatCustomer(Long id, StatCustomer statCustomer) {
+        Customer customer = customerRepo.getById(id).orElseThrow(() -> new CustomerNotFoundException(id));
+        if (isInvalidRequest(customer, statCustomer)) {
+            statCustomer.setValid(false);
+            return statRepo.save(statCustomer);
+        }
+        statCustomer.setValid(true);
         return statRepo.save(statCustomer);
     }
 
@@ -35,8 +53,15 @@ public class StatService {
         return statRepo.findAllStatCustomersByCustomerIdAndIsValidAndTimeBetween(id, isValid, dateFrom, dateTo);
     }
 
-      //TODO
-    /*public boolean isValidRequest(StatCustomer statCustomer) {
-        return true;
-    }*/
+    public boolean isInvalidRequest(Customer customer, StatCustomer statCustomer) {
+        boolean blackListUserAgent = blackListUARepo.existsBlackListUserAgentByUa(statCustomer.getUserAgent());
+        boolean blackListIP = blackListIPRepo.existsBlackListIPByIp(statCustomer.getRemoteIp());
+
+        return  (customer.getStatus() == 0)
+                || (blackListUserAgent)
+                || (blackListIP)
+                || (statCustomer.getUserAgent() == null)
+                || statCustomer.getUserAgent().equals("")
+                || (statCustomer.getRemoteIp() == null);
+    }
 }
