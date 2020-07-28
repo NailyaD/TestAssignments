3a89201ad115058f5c80d0d00e1797e41731fd2e
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-07-28 21:56:31 +0200

    Contains only get mapping methods

--- src/main/java/org/homeprog/customerrequestsstat/controller/StatController.java
+++ src/main/java/org/homeprog/customerrequestsstat/controller/StatController.java
@@ -4,9 +4,10 @@ import org.homeprog.customerrequestsstat.DTO.StatCustomerDTO;
 import org.homeprog.customerrequestsstat.entity.StatCustomer;
 import org.homeprog.customerrequestsstat.service.StatService;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -25,24 +26,13 @@ public class StatController {
     public StatController(StatService service, ModelMapper modelMapper) {
         this.service = service;
         this.modelMapper = modelMapper;
-        this.modelMapper.addMappings(statCustomerMapping);
-        this.modelMapper.addMappings(statCustomerIdMapping);
-    }
-
-    @PostMapping("/api/customers")
-    public StatCustomer addStatCustomer(@RequestBody StatCustomerDTO statCustomerDTO) {
-        StatCustomer statCustomer = modelMapper.map(statCustomerDTO, StatCustomer.class);
-        Long customerId = statCustomerDTO.getCustomerId();
-        return service.addStatCustomer(customerId, statCustomer);
     }
 
     //Date parameters shall be passed as "yyyy-MM-dd HH:mm:ss"
     @GetMapping("/api/stats/{customer_id}/from/{date_from}/to/{date_to}")
     public List<StatCustomerDTO> getCustomersByCustomerIdAndTimePeriod(@PathVariable("customer_id") long id,
-                                                                 @PathVariable("date_from")
-                                                                 String fromDate,
-                                                                 @PathVariable("date_to")
-                                                                 String toDate) {
+                                                                       @PathVariable("date_from") String fromDate,
+                                                                       @PathVariable("date_to") String toDate) {
         Instant instantFrom = stringToInstantConverter(fromDate);
         Instant instantTo = stringToInstantConverter(toDate);
 
@@ -79,18 +69,6 @@ public class StatController {
         return  statCustomers.stream().map(customer -> modelMapper
                 .map(customer, StatCustomerDTO.class)).collect(Collectors.toList());
     }
-
-    PropertyMap<StatCustomerDTO, StatCustomer> statCustomerMapping = new PropertyMap<StatCustomerDTO, StatCustomer>() {
-        protected void configure() {
-            map().getCustomer().setId(source.getCustomerId());
-        }
-    };
-
-    PropertyMap<StatCustomer, StatCustomerDTO> statCustomerIdMapping = new PropertyMap<StatCustomer, StatCustomerDTO>() {
-        protected void configure() {
-            map().setCustomerId(source.getCustomer().getId());
-        }
-    };
 
     public Instant stringToInstantConverter(String string) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
